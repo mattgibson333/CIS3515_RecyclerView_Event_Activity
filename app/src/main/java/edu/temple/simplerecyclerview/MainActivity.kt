67388e882f4c946d1f9e35f728da80affdb3e0ec
Matt Gibson <mattgibson333@gmail.com>
2024-09-26 11:45:36 -0400

    TODO 2: Lambda defined for displayTextView

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
-
+        val callBack = {index: Int -> displayTextView.textSize = index.toFloat()}
         // Todo (Step 3: Pass lambda to adapter)
         recyclerView.adapter = NumberDisplayAdapter(numbers)
 
